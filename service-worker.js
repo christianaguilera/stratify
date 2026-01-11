bea9f7a908f8de36910c6c1921310ad7c1825570
christianaguilera <christian.aguileracardenas@gmail.com>
2026-01-11 20:47:11 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'reportes-v20';
+const CACHE_NAME = 'reportes-v21';
 const FILES_TO_CACHE = [
   '/stratify/',
   '/stratify/index.html',
@@ -39,6 +39,7 @@ self.addEventListener('fetch', event => {
 self.addEventListener('message', event => {
   if (event.data === 'SKIP_WAITING') self.skipWaiting();
 });
+
 
 
 
